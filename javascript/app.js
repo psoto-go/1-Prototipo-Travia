cf2b5cae86162a4cf16edbb45a6b306e8628aebb
AcostaLa <soylautaroacosta@gmail.com>
2021-08-05 16:28:07 -0300

    detalles del index y detalles de js

--- javascript/app.js
+++ javascript/app.js
@@ -2,12 +2,22 @@ scrollTopButton('.btn-subir')
 
 function scrollTopButton(btn){
     const ScrollBtn = $(btn);
-    console.log(ScrollBtn);
 
 
     $(window).scroll(function(){
         let scrollTop = $(this).scrollTop();
         scrollTop > 700 ? ScrollBtn.removeClass('hidden') : ScrollBtn.addClass('hidden')
+    })
+
+    $ScrollBtm.click(function(){
+    // '.btn-subir' -> 'btn-subir'
+    let btnClass = btn.substr(1);
+    if($(this).hasClass(btnClass)){
+        window.scrollTo({
+            behavior: 'smooth',
+            top: 0
+        })
+    }
     })
 
 }
